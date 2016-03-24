87c39d147179eef71be3ea885e09023a2f2403a8
Diego Moura <diegosouza20049@hotmail.com>
2016-03-24 16:04:32 -0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">one</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">three</a></li>
+</ul>
+<nav>
+  <div class="nav-wrapper blue darken-2">
+    <a href="#!" class="brand-logo"> DogChic</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="sass.html">Sass</a></li>
+      <li><a href="badges.html">Components</a></li>
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
+  <div class="container"></div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App'
+import Home	from './components/Hello'
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
